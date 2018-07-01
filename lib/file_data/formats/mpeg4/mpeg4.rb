8ffa60f8a2c24f2f83c7c9373f8f32014e066c76
Scott Haney <scotthaney323@gmail.com>
2018-07-01 04:02:40 +0000

    Remove extra code from mpeg4.rb and get its unit tests to pass

--- lib/file_data/formats/mpeg4/mpeg4.rb
+++ lib/file_data/formats/mpeg4/mpeg4.rb
@@ -1,6 +1,8 @@
-require_relative 'box_stream'
+require_relative 'boxes_reader'
 require_relative '../../core_extensions/binary_extensions'
 require 'date'
+require_relative 'box_parsers/ilst_box'
+require_relative 'box_parsers/keys_box'
 
 module FileData
   # Parses and returns metadata from an Mpeg4 file
@@ -11,57 +13,10 @@ module FileData
       ['.mp4', '.mpeg4'].each { |e| File.info_maps[e] = self }
     end
 
-    def self.ilst_box(stream)
-      size = read_value(4, stream)
-      index = read_value(4, stream)
-      data_box = get_ilst_data_box(stream, size)
-
-      data_type = read_value(4, stream)
-      locale = read_value(4, stream)
-      value = read_ascii(data_box.content_size - 8, stream)
-
-      IlstBox.new(index, data_type, locale, value)
-    end
-
-    def self.get_ilst_data_box(stream, size)
-      BoxesReader.for_position(stream, stream.pos, size - 8)
-                 .boxes.find { |box| box.type == 'data' }
-    end
-
-    def self.keys_box(stream)
-      read_value(1, stream) # version field
-      read_value(3, stream) # flags field
-
-      entry_count = read_value(4, stream)
-
-      Array.new(entry_count) do |index|
-        size = read_value(4, stream)
-        namespace = read_ascii(4, stream)
-        value = read_ascii(size - 8, stream)
-
-        Key.new(index + 1, namespace, value)
-      end
-    end
-
     def self.origin_date(stream)
-      meta_box = get_root_path(stream, 'moov', 'meta')
-
-      get_box_path(stream, meta_box, 'keys')
-      keys = keys_box(stream)
-
-      creation_key = keys.find { |key| key.value == 'com.apple.quicktime.creationdate' }
-
-      ilst_boxes = get_ilst_boxes(stream, meta_box)
-
-      creation_date_data = ilst_boxes.find { |x| x.index == creation_key.index }
-      Time.parse(creation_date_data.value_text)
-    end
-
-    def self.get_ilst_boxes(stream, meta_box)
-      box = get_box_path(stream, meta_box, 'ilst')
-      ilst_boxes = []
-      ilst_boxes << ilst_box(stream) while stream.pos < box.content_pos + box.content_size
-      ilst_boxes
+      mb = get_root_path(stream, 'moov', 'meta')
+      meta_box = MetaBoxParser.parse(mb.content_stream)
+      meta_box.creation_date
     end
 
     def self.creation_date(stream)
@@ -79,28 +34,23 @@ module FileData
     end
 
     def self.get_root_path(stream, *box_path)
-      reader = FileData::BoxesReader.for_file(stream)
-      get_path(stream, reader, *box_path)
+      get_path(Helpers::StreamView.new(stream), *box_path)
     end
 
-    def self.get_box_path(stream, box, *box_path)
-      reader = FileData::BoxesReader.for_box(stream, box)
-      get_path(stream, reader, *box_path)
+    def self.get_box_path(box, *box_path)
+      get_path(box.content_stream, *box_path)
     end
 
-    def self.get_path(stream, reader, *box_path)
-      match = reader.boxes.find { |x| x.type == box_path[0] }
-
+    def self.get_path(stream_view, *box_path)
+      match = BoxesReader.read(stream_view).find { |x| x.type == box_path[0] }
+      
       if match.nil?
         nil
       elsif box_path.length == 1
         match
       else
-        get_path(stream, FileData::BoxesReader.for_box(stream, match), *box_path[1..-1])
+        get_path(match.content_stream, *box_path[1..-1])
       end
     end
   end
-
-  IlstBox = Struct.new(:index, :data_type, :locale, :value_text)
-  Key = Struct.new(:index, :namespace, :value)
 end
