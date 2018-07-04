48c06c9c19ef9b47f010f4f1179a57d86d30a7f9
Scott Haney <scotthaney323@gmail.com>
2018-07-04 11:58:17 +0000

    Move box searching logic out of mpeg4.rb and into box_path.rb

--- lib/file_data/formats/mpeg4/box_path.rb
+++ lib/file_data/formats/mpeg4/box_path.rb
@@ -2,15 +2,23 @@ require_relative 'boxes_reader'
 
 module FileData
   class BoxPath
+    def self.get_root_path(stream, *box_path)
+      get_path(Helpers::StreamView.new(stream), *box_path)
+    end
+
+    # def self.get_box_path(box, *box_path)
+    #   get_path(box.content_stream, *box_path)
+    # end
+
     def self.get_path(stream_view, *box_path)
       match = BoxesReader.read(stream_view).find { |x| x.type == box_path[0] }
 
       if match.nil?
-          nil
+        nil
       elsif box_path.length == 1
-          match
+        match
       else
-          get_path(match.content_stream, *box_path[1..-1])
+        get_path(match.content_stream, *box_path[1..-1])
       end
     end
   end
--- lib/file_data/formats/mpeg4/mpeg4.rb
+++ lib/file_data/formats/mpeg4/mpeg4.rb
@@ -1,8 +1,6 @@
-require_relative 'boxes_reader'
+require_relative 'box_path'
 require_relative '../../core_extensions/binary_extensions'
-require 'date'
-require_relative 'box_parsers/ilst_box'
-require_relative 'box_parsers/keys_box'
+require_relative 'box_parsers/meta_box'
 require_relative 'box_parsers/mvhd_box'
 
 module FileData
@@ -15,7 +13,7 @@ module FileData
     end
 
     def self.origin_date(stream)
-      mb = get_root_path(stream, 'moov', 'meta')
+      mb = BoxPath.get_root_path(stream, 'moov', 'meta')
 
       if mb.nil?
         nil
@@ -25,32 +23,12 @@ module FileData
     end
 
     def self.creation_date(stream)
-      box = get_root_path(stream, 'moov', 'mvhd')
+      box = BoxPath.get_root_path(stream, 'moov', 'mvhd')
 
       if box.nil?
         nil
       else
         MvhdBoxParser.parse(box.content_stream).creation_time
-      end
-    end
-
-    def self.get_root_path(stream, *box_path)
-      get_path(Helpers::StreamView.new(stream), *box_path)
-    end
-
-    def self.get_box_path(box, *box_path)
-      get_path(box.content_stream, *box_path)
-    end
-
-    def self.get_path(stream_view, *box_path)
-      match = BoxesReader.read(stream_view).find { |x| x.type == box_path[0] }
-
-      if match.nil?
-        nil
-      elsif box_path.length == 1
-        match
-      else
-        get_path(match.content_stream, *box_path[1..-1])
       end
     end
   end
