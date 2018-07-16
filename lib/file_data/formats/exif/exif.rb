101a64c6364ba256139a32bdcdc50799b44c782a
Scott Haney <scotthaney323@gmail.com>
2018-07-16 02:34:57 +0000

    Revert time handling to the DateTime class in exif.rb since it seems to be more robust at correctly parsing tags

--- lib/file_data/formats/exif/exif.rb
+++ lib/file_data/formats/exif/exif.rb
@@ -34,7 +34,7 @@ module FileData
 
     def self.creation_date(input)
       raw_tag = FileData::Exif.only_image_tag(input, [34_665, 36_867])
-      raw_tag.nil? ? nil : Time.parse(raw_tag)
+      DateTime.strptime(raw_tag, '%Y:%m:%d %H:%M:%S') unless raw_tag.nil?
     end
 
     def self.origin_date(input)
