dd708b3fdb6cc0ebd4691d5ea774d7e175092fa3
Scott Haney <scotthaney323@gmail.com>
2018-07-20 21:13:50 +0000

    Add a test for FileInfo.origin_date using an Exif file

--- spec/file_info_spec.rb
+++ spec/file_info_spec.rb
@@ -1,9 +1,11 @@
-require 'support/test_stream'
+require 'fakefs/spec_helpers'
 require 'file_data/file_types/file_info'
 require 'file_data/formats/exif/exif'
 require 'file_data/formats/mpeg4/mpeg4'
 
 RSpec.describe FileData::FileInfo do
+  include FakeFS::SpecHelpers
+
   let(:file_info) { FileData::FileInfo }
 
   describe '.can_handle?' do
@@ -27,6 +29,41 @@ RSpec.describe FileData::FileInfo do
       let(:filename) { 'test.oiuj@%owfda2' }
       it 'returns false' do
         expect(can_handle).to be false
+      end
+    end
+  end
+
+  describe '.origin_date' do
+    let(:origin_date) { file_info.origin_date(filename) }
+
+    context 'given a jpeg file with a creation date of 2018:01:20 12:00:00' do
+      let(:filename) { '/test.jpg' }
+      let(:test_bytes) do
+        [255, 216, # JPEG SOI
+         [255, 225], [0, 73], # APP1 marker and size
+         [69, 120, 105, 102, 0, 0], # Exif\0\0 marker
+         [77, 77], [0, 42], # Exif big endian header
+         [0, 0, 0, 8], [0, 1], # IFD0 offset and tag count
+         [135, 105], [0, 4], [0, 0, 0, 4], [0, 0, 0, 26],
+         [0, 0, 0, 0], #No next IFD marker
+         [0, 1], #Number of IFD tags
+         [144, 3], [0, 2], [0, 0, 0, 19], [0, 0, 0, 44], #Creation Date Tag
+         [0, 0, 0, 0], # No next IFD marker
+         [50, 48, 49, 56, 58, 48, 49, 58, 50, 48, 32, 49, 50, 58, 48, 48, 58, 48, 48], #Creation Date Value '2018:01:20 12:00:00'
+         [255, 217]].flatten #JPEG EOI
+      end
+
+      before :example do
+        FileUtils.touch filename
+        File.open(filename, 'w') do |output|
+          test_bytes.each do |byte|
+            output.print byte.chr
+          end
+        end
+      end
+
+      it 'extracts the creation date from the jpeg file' do
+        expect(origin_date).to eq(DateTime.strptime('2018:01:20 12:00:00', '%Y:%m:%d %H:%M:%S'))
       end
     end
   end
